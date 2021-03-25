dec881115d05e587c2a23f1ace524d5e80fa888c
Lorenzo Pisani <zeelot3k@gmail.com>
2021-03-24 21:28:14 -0700

    initial pygame window

---
+++ src/seagulls/_asset_manager.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from pygame import Surface
+from pygame.image import load
+
+
+class AssetManager:
+    _assets_path: Path
+
+    def __init__(self, assets_path: Path):
+        self._assets_path = assets_path
+
+    def load_sprite(self, name: str, with_alpha: bool = True) -> Surface:
+        path = self._assets_path / f"sprites/{name}.png"
+        loaded_sprite = load(path.resolve())
+
+        if with_alpha:
+            return loaded_sprite.convert_alpha()
+        else:
+            return loaded_sprite.convert()
--- src/seagulls/_cli/_entrypoint.py
+++ src/seagulls/_cli/_entrypoint.py
@@ -1,2 +1,53 @@
+from pathlib import Path
+
+import pygame
+
+from seagulls._asset_manager import AssetManager
+
+
+class Seagulls:
+
+    _screen: pygame.Surface
+    _background: pygame.Surface
+    _asset_manager: AssetManager
+
+    def __init__(self):
+        self._init_pygame()
+        self.screen = pygame.display.set_mode((800, 600))
+        self._asset_manager = AssetManager(Path("assets"))
+        self._background = self._asset_manager.load_sprite("background", False)
+
+    def main_loop(self):
+        while True:
+            self._handle_input()
+            self._process_game_logic()
+            self._draw()
+
+    def _init_pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Seagulls")
+
+    def _handle_input(self):
+        for event in pygame.event.get():
+            if self._should_quit(event):
+                quit()
+
+    def _should_quit(self, event):
+        if event.type == pygame.QUIT:
+            return True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            return True
+
+        return False
+
+    def _process_game_logic(self):
+        pass
+
+    def _draw(self):
+        self.screen.blit(self._background, (0, 0))
+        pygame.display.flip()
+
+
 def main():
-    print("Woo!")
+    game = Seagulls()
+    game.main_loop()

52502eff75556267f79110a0afae5ce26aadd34b
Lorenzo Pisani <zeelot3k@gmail.com>
2021-04-11 16:09:46 -0700

    added some example files we can run to explain python basics

---
+++ src/seagulls/examples/_classes.py
@@ -0,0 +1,9 @@
+class SomeClass:
+    def some_method(self):
+        print("called some_method() in the SomeClass class")
+
+
+if __name__ == "__main__":
+    print("example classes")
+    thing = SomeClass()
+    thing.some_method()

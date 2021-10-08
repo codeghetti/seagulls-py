cddd564b6350e345e2e01cade060780aa0d8090a
Lorenzo Pisani <zeelot3k@gmail.com>
2021-10-07 23:42:06 -0700

    remove useless cli argument

--- src/seagulls/cli/_launch_command.py
+++ src/seagulls/cli/_launch_command.py
@@ -23,7 +23,7 @@ class LaunchCommand(CliCommand):
         return "Launch the _seagulls game."
 
     def configure_parser(self, parser: ArgumentParser) -> None:
-        parser.add_argument("scene", help="name of scene to load")
+        pass
 
     def execute(self, args: Dict[str, Any]):
         try:

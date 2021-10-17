7bc9c0468e43fbbc5a89c0e6650192278fafb6d7
Jennifer <jenniferdeeplearns@gmail.com>
2021-10-16 22:20:02 -0700

    Added functionality to create random RPG scenes

--- src/seagulls/examples/_simple_rpg_background.py
+++ src/seagulls/examples/_simple_rpg_background.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import random
 
 from seagulls.assets import AssetManager
 from seagulls.engine import GameObject, Surface
@@ -20,4 +21,68 @@ class SimpleRpgBackground(GameObject):
 
     @lru_cache()
     def _get_cached_background(self) -> Surface:
-        return self._asset_manager.load_sprite("environment/environment-rpg").copy()
+        surface = Surface((1024, 600))
+
+        top_left_corner = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-top-left-corner").copy()
+        top_island_edge = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-top-edge").copy()
+        top_right_corner = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-top-right-corner").copy()
+        island_water = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-water").copy()
+        bottom_left_corner = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-bottom-left-corner").copy()
+        bottom_island_edge = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-bottom-edge").copy()
+        bottom_right_corner = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-bottom-right-corner").copy()
+        island_left_edge = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-left-edge").copy()
+        island_right_edge = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-right-edge").copy()
+        island_grass = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-grass").copy()
+        island_red_home = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-red-home").copy()
+        island_blue_home = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-blue-home").copy()
+        island_tree = self._asset_manager.load_sprite(
+            "environment/rpg-environment/island-tree").copy()
+
+        for y in range(int(600 / 16)):
+            for x in range(int(1024 / 16)):
+                if y == 0:
+                    if x == 0:
+                        surface.blit(top_left_corner, (x * 16, y * 16))
+                    elif x == 63:
+                        surface.blit(top_right_corner, (x * 16, y * 16))
+                    else:
+                        surface.blit(top_island_edge, (x * 16, y * 16))
+                elif y == 35:
+                    if x == 0:
+                        surface.blit(bottom_left_corner, (x * 16, y * 16))
+                    elif x == 63:
+                        surface.blit(bottom_right_corner, (x * 16, y * 16))
+                    else:
+                        surface.blit(bottom_island_edge, (x * 16, y * 16))
+                elif y == 36:
+                    surface.blit(island_water, (x * 16, y * 16))
+
+                elif x == 0:
+                    surface.blit(island_left_edge, (x * 16, y * 16))
+
+                elif x == 63:
+                    surface.blit(island_right_edge, (x * 16, y * 16))
+
+                else:
+                    random_number = random.randint(0, 100)
+                    if random_number < 75:
+                        surface.blit(island_grass, (x * 16, y * 16))
+                    elif random_number < 78:
+                        surface.blit(island_red_home, (x * 16, y * 16))
+                    elif random_number < 80:
+                        surface.blit(island_blue_home, (x * 16, y * 16))
+                    else:
+                        surface.blit(island_tree, (x * 16, y * 16))
+        return surface

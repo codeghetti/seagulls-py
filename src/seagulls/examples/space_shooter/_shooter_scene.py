b44e7594617e25addd04596af8d7f75434dde74f
Lorenzo Pisani <zeelot3k@gmail.com>
2021-11-27 23:34:11 -0800

    initial refactor to move logic out of space collisions class

--- src/seagulls/examples/space_shooter/_shooter_scene.py
+++ src/seagulls/examples/space_shooter/_shooter_scene.py
@@ -1,5 +1,13 @@
 import logging
+from abc import abstractmethod, ABC
+from enum import Enum, auto
+from functools import lru_cache
+from pathlib import Path
 from threading import Event
+from typing import Tuple
+
+from pygame import mixer
+from pygame.font import Font
 
 from seagulls.assets import AssetManager
 from seagulls.engine import (
@@ -11,8 +19,170 @@ from seagulls.engine import (
     Surface,
     SurfaceRenderer
 )
+from ._asteroid_field import AsteroidField
+from ._ship import Ship
 
 logger = logging.getLogger(__name__)
+
+
+class ShooterSceneState(Enum):
+    RUNNING = auto()
+    WON = auto()
+    LOST = auto()
+
+
+class ShooterSceneStateClient:
+    _current_state: ShooterSceneState
+
+    def __init__(self):
+        self._current_state = ShooterSceneState.RUNNING
+
+    def update_state(self, state: ShooterSceneState) -> None:
+        self._current_state = state
+
+    def get_state(self) -> ShooterSceneState:
+        return self._current_state
+
+
+class ICheckGameRules(ABC):
+
+    @abstractmethod
+    def check(self) -> None:
+        pass
+
+
+class AsteroidMissedRule(ICheckGameRules):
+    _state_client: ShooterSceneStateClient
+    _asteroid_field: AsteroidField
+
+    def __init__(self, state_client: ShooterSceneStateClient, asteroid_field: AsteroidField):
+        self._state_client = state_client
+        self._asteroid_field = asteroid_field
+
+    def check(self) -> None:
+        for x in range(self._asteroid_field.get_asteroid_field_size()):
+            if self._asteroid_field.get_rock_position_y(x) > 600:
+                self._state_client.update_state(ShooterSceneState.LOST)
+
+
+class ShipDestroyedRule(ICheckGameRules):
+    _state_client: ShooterSceneStateClient
+    _asteroid_field: AsteroidField
+    _ship: Ship
+
+    def __init__(
+            self,
+            state_client: ShooterSceneStateClient,
+            asteroid_field: AsteroidField,
+            ship: Ship):
+        self._state_client = state_client
+        self._asteroid_field = asteroid_field
+        self._ship = ship
+
+    def check(self) -> None:
+        for index in range(self._asteroid_field.get_asteroid_field_size()):
+            if self._ship_rock_collision_check(index):
+                self._state_client.update_state(ShooterSceneState.LOST)
+                return
+
+    def _ship_rock_collision_check(self, rock_number: int) -> bool:
+        ship_position = self._ship.get_ship_position()
+
+        nose_collision_check = (
+            self._asteroid_field.get_rock_position_x(rock_number) <= (ship_position.x + 50) <=
+            self._asteroid_field.get_rock_position_x(rock_number) +
+            self._asteroid_field.get_rock_size_x(rock_number) and
+            self._asteroid_field.get_rock_position_y(rock_number) <=
+            ship_position.y <=
+            self._asteroid_field.get_rock_position_y(rock_number) +
+            self._asteroid_field.get_rock_size_y(rock_number)
+        )
+
+        left_upper_wing_check = (
+            self._asteroid_field.get_rock_position_x(rock_number) <= ship_position.x <=
+            self._asteroid_field.get_rock_position_x(rock_number) +
+            self._asteroid_field.get_rock_size_x(rock_number) and
+            self._asteroid_field.get_rock_position_y(rock_number) <=
+            (ship_position.y + 30) <=
+            self._asteroid_field.get_rock_position_y(rock_number) +
+            self._asteroid_field.get_rock_size_y(rock_number)
+        )
+
+        right_upper_wing_check = (
+            self._asteroid_field.get_rock_position_x(rock_number) <= (ship_position.x + 105) <=
+            self._asteroid_field.get_rock_position_x(rock_number) +
+            self._asteroid_field.get_rock_size_x(rock_number) and
+            self._asteroid_field.get_rock_position_y(rock_number) <=
+            (ship_position.y + 30) <=
+            self._asteroid_field.get_rock_position_y(rock_number) +
+            self._asteroid_field.get_rock_size_y(rock_number)
+        )
+
+        left_lower_wing_check = (
+            self._asteroid_field.get_rock_position_x(rock_number) <= ship_position.x <=
+            self._asteroid_field.get_rock_position_x(rock_number) +
+            self._asteroid_field.get_rock_size_x(rock_number)
+            and self._asteroid_field.get_rock_position_y(rock_number) <=
+            (ship_position.y + 68) <=
+            self._asteroid_field.get_rock_position_y(rock_number) +
+            self._asteroid_field.get_rock_size_y(rock_number)
+        )
+
+        right_lower_wing_check = (
+            self._asteroid_field.get_rock_position_x(rock_number) <= (ship_position.x + 105) <=
+            self._asteroid_field.get_rock_position_x(rock_number) +
+            self._asteroid_field.get_rock_size_x(rock_number) and
+            self._asteroid_field.get_rock_position_y(rock_number) <=
+            (ship_position.y + 68) <=
+            self._asteroid_field.get_rock_position_y(rock_number) +
+            self._asteroid_field.get_rock_size_y(rock_number)
+        )
+
+        return (
+            nose_collision_check or
+            left_lower_wing_check or
+            right_lower_wing_check or
+            left_upper_wing_check or
+            right_upper_wing_check)
+
+
+class GameOverOverlay(GameObject):
+
+    _font: Font
+
+    def __init__(self):
+        self._font = Font(Path("assets/fonts/ubuntu-mono-v10-latin-regular.ttf"), 50)
+
+    def tick(self) -> None:
+        pass
+
+    def render(self, surface: Surface) -> None:
+        img = self._font.render(
+            "GAME OVER",
+            True,
+            "red", "black"
+        )
+        surface.blit(img, (380, 260))
+
+
+class ToggleableGameObject(GameObject):
+    _game_object: GameObject
+    _active: bool
+
+    def __init__(self, game_object: GameObject):
+        self._game_object = game_object
+        self._active = True
+
+    def tick(self) -> None:
+        if self._active:
+            self._game_object.tick()
+
+    def render(self, surface: Surface) -> None:
+        if self._active:
+            self._game_object.render(surface)
+
+    def toggle(self) -> None:
+        self._active = not self._active
 
 
 class ShooterScene(IGameScene):
@@ -24,16 +194,22 @@ class ShooterScene(IGameScene):
     _game_objects: GameObjectsCollection
     _should_quit: Event
 
+    _state_client: ShooterSceneStateClient
+    _game_rules: Tuple[ICheckGameRules, ...]
+
+    _toggleables: Tuple[ToggleableGameObject, ...]
+
     def __init__(
             self,
             clock: GameClock,
             surface_renderer: SurfaceRenderer,
             asset_manager: AssetManager,
             background: GameObject,
-            ship: GameObject,
-            asteroid_field: GameObject,
+            ship: Ship,
+            asteroid_field: AsteroidField,
             space_collisions: GameObject,
             game_controls: GameControls):
+        mixer.init()
         self._surface_renderer = surface_renderer
         self._asset_manager = asset_manager
         self._game_controls = game_controls
@@ -42,9 +218,21 @@ class ShooterScene(IGameScene):
         self._game_objects.add(clock)
         self._game_objects.add(background)
         self._game_objects.add(ship)
-        self._game_objects.add(asteroid_field)
         self._game_objects.add(space_collisions)
         self._game_objects.add(self._game_controls)
+
+        self._toggleables = tuple([
+            ToggleableGameObject(asteroid_field)
+        ])
+
+        self._state_client = ShooterSceneStateClient()
+        self._game_rules = tuple([
+            AsteroidMissedRule(self._state_client, asteroid_field),
+            ShipDestroyedRule(self._state_client, asteroid_field, ship),
+        ])
+
+        for item in self._toggleables:
+            self._game_objects.add(item)
 
         self._should_quit = Event()
 
@@ -62,7 +250,20 @@ class ShooterScene(IGameScene):
             logger.debug("QUIT EVENT DETECTED")
             self._should_quit.set()
 
+        for rule in self._game_rules:
+            rule.check()
+
+        if not self._state_client.get_state() == ShooterSceneState.RUNNING:
+            self._end_game()
+
         self._render()
+
+    @lru_cache()
+    def _end_game(self) -> None:
+        mixer.Sound("assets/sounds/game-over.ogg").play()
+        self._game_objects.add(GameOverOverlay())
+        for item in self._toggleables:
+            item.toggle()
 
     def _render(self) -> None:
         background = Surface((1024, 600))

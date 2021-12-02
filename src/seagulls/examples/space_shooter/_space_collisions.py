e15e558be2632fb891be79026121ff4426e2af38
Z <zeelot3k@gmail.com>
2021-12-01 21:00:31 -0800

    initial refactor to move logic out of space collisions class (#61)

--- src/seagulls/examples/space_shooter/_space_collisions.py
+++ src/seagulls/examples/space_shooter/_space_collisions.py
@@ -1,7 +1,5 @@
 import logging
 from pathlib import Path
-
-from pygame import mixer
 from pygame.font import Font
 
 from seagulls.engine import GameObject, Surface
@@ -23,23 +21,10 @@ class SpaceCollisions(GameObject):
         self._asteroid_field = asteroid_field
         self._collision_count = 0
         self._font = Font(Path("assets/fonts/ubuntu-mono-v10-latin-regular.ttf"), 18)
-        self._game_over_font = Font(Path("assets/fonts/ubuntu-mono-v10-latin-regular.ttf"), 50)
-        mixer.init()
-        self._is_game_over = False
 
     def tick(self) -> None:
         _remove_lasers = []
         _remove_rocks = []
-
-        for rock in range(self._asteroid_field.get_asteroid_field_size()):
-            if self._ship_rock_collision_check(rock):
-                self._end_game()
-                return
-
-        for rock in range(self._asteroid_field.get_asteroid_field_size()):
-            if self._asteroid_field.get_rock_position_y(rock) > 600:
-                self._end_game()
-                return
 
         for laser in range(self._ship.get_number_of_lasers()):
             for rock in range(self._asteroid_field.get_asteroid_field_size()):
@@ -55,12 +40,6 @@ class SpaceCollisions(GameObject):
         for rock in _remove_rocks:
             self._asteroid_field.remove_rock(rock)
 
-    def _end_game(self):
-        mixer.Sound("assets/sounds/game-over.ogg").play()
-        self._asteroid_field.set_game_over()
-        self._remove_all_rocks()
-        self._is_game_over = True
-
     def render(self, surface: Surface) -> None:
         img = self._font.render(
             f"Score: {self._collision_count}",
@@ -68,81 +47,6 @@ class SpaceCollisions(GameObject):
             "red", "black"
         )
         surface.blit(img, (920, 570))
-
-        if self._is_game_over:
-            game_over_img = self._game_over_font.render(
-                "GAME OVER",
-                True,
-                "red", "black"
-            )
-            surface.blit(game_over_img, (380, 260))
-
-    def _ship_rock_collision_check(self, rock_number: int) -> bool:
-        ship_position = self._ship.get_ship_position()
-
-        nose_collision_check = (
-            self._asteroid_field.get_rock_position_x(rock_number) <= (ship_position.x + 50) <=
-            self._asteroid_field.get_rock_position_x(rock_number) +
-            self._asteroid_field.get_rock_size_x(rock_number) and
-            self._asteroid_field.get_rock_position_y(rock_number) <=
-            ship_position.y <=
-            self._asteroid_field.get_rock_position_y(rock_number) +
-            self._asteroid_field.get_rock_size_y(rock_number)
-        )
-
-        left_upper_wing_check = (
-            self._asteroid_field.get_rock_position_x(rock_number) <= ship_position.x <=
-            self._asteroid_field.get_rock_position_x(rock_number) +
-            self._asteroid_field.get_rock_size_x(rock_number) and
-            self._asteroid_field.get_rock_position_y(rock_number) <=
-            (ship_position.y + 30) <=
-            self._asteroid_field.get_rock_position_y(rock_number) +
-            self._asteroid_field.get_rock_size_y(rock_number)
-        )
-
-        right_upper_wing_check = (
-            self._asteroid_field.get_rock_position_x(rock_number) <= (ship_position.x + 105) <=
-            self._asteroid_field.get_rock_position_x(rock_number) +
-            self._asteroid_field.get_rock_size_x(rock_number) and
-            self._asteroid_field.get_rock_position_y(rock_number) <=
-            (ship_position.y + 30) <=
-            self._asteroid_field.get_rock_position_y(rock_number) +
-            self._asteroid_field.get_rock_size_y(rock_number)
-        )
-
-        left_lower_wing_check = (
-            self._asteroid_field.get_rock_position_x(rock_number) <= ship_position.x <=
-            self._asteroid_field.get_rock_position_x(rock_number) +
-            self._asteroid_field.get_rock_size_x(rock_number)
-            and self._asteroid_field.get_rock_position_y(rock_number) <=
-            (ship_position.y + 68) <=
-            self._asteroid_field.get_rock_position_y(rock_number) +
-            self._asteroid_field.get_rock_size_y(rock_number)
-        )
-
-        right_lower_wing_check = (
-            self._asteroid_field.get_rock_position_x(rock_number) <= (ship_position.x + 105) <=
-            self._asteroid_field.get_rock_position_x(rock_number) +
-            self._asteroid_field.get_rock_size_x(rock_number) and
-            self._asteroid_field.get_rock_position_y(rock_number) <=
-            (ship_position.y + 68) <=
-            self._asteroid_field.get_rock_position_y(rock_number) +
-            self._asteroid_field.get_rock_size_y(rock_number)
-        )
-
-        return (
-            nose_collision_check or
-            left_lower_wing_check or
-            right_lower_wing_check or
-            left_upper_wing_check or
-            right_upper_wing_check)
-
-    def _ship_rock_collision_check_y(self, rock_number: int) -> bool:
-        pos = self._ship.get_ship_position()
-        return self._asteroid_field.get_rock_position_y(rock_number) <= \
-            (pos.y + 37) <= \
-            self._asteroid_field.get_rock_position_y(rock_number) + \
-            self._asteroid_field.get_rock_size_y(rock_number)
 
     def _laser_rock_collision_check_x(self, laser_number: int, rock_number: int) -> bool:
         return self._asteroid_field.get_rock_position_x(rock_number) <= \
@@ -155,7 +59,3 @@ class SpaceCollisions(GameObject):
                             self._ship.get_laser_position_y(laser_number) <= \
                             self._asteroid_field.get_rock_position_y(rock_number) + \
                             self._asteroid_field.get_rock_size_y(rock_number)
-
-    def _remove_all_rocks(self) -> None:
-        for rock in range(self._asteroid_field.get_asteroid_field_size()):
-            self._asteroid_field.remove_rock(0)

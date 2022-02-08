0c6cb1cc8f42136eba5c24eec11b26028fc965e4
Lorenzo Pisani <zeelot3k@gmail.com>
2022-02-07 22:10:06 -0800

    add a cli command to publish the wheel package

--- seagulls-devtools/src/seagulls/devtools/_cli_plugin.py
+++ seagulls-devtools/src/seagulls/devtools/_cli_plugin.py
@@ -4,6 +4,7 @@ from seagulls.cli import CliRequestRegistrationEvent
 from seagulls.seagulls_cli import SeagullsCliApplication
 from ._flake8_command import Flake8Command
 from ._mypy_command import MypyCommand
+from ._publish_command import PublishWheelCommand
 from ._pytest_command import PytestCommand
 
 
@@ -13,17 +14,20 @@ class DevtoolsCliPlugin(ISeagullsApplicationPlugin):
     _flake8_command: Flake8Command
     _mypy_command: MypyCommand
     _pytest_command: PytestCommand
+    _publish_command: PublishWheelCommand
 
     def __init__(
             self,
             application: SeagullsCliApplication,
             flake8_command: Flake8Command,
             mypy_command: MypyCommand,
-            pytest_command: PytestCommand):
+            pytest_command: PytestCommand,
+            publish_command: PublishWheelCommand):
         self._application = application
         self._flake8_command = flake8_command
         self._mypy_command = mypy_command
         self._pytest_command = pytest_command
+        self._publish_command = publish_command
 
     def on_registration(self) -> None:
         self._application.register_callback(
@@ -34,3 +38,4 @@ class DevtoolsCliPlugin(ISeagullsApplicationPlugin):
         event.register_command("flake8", self._flake8_command)
         event.register_command("mypy", self._mypy_command)
         event.register_command("pytest", self._pytest_command)
+        event.register_command("publish", self._publish_command)
---
+++ seagulls-devtools/src/seagulls/devtools/_publish_command.py
@@ -0,0 +1,26 @@
+import shutil
+import subprocess
+from pathlib import Path
+import logging
+from argparse import ArgumentParser
+
+from seagulls.cli import ICliCommand
+
+logger = logging.getLogger(__name__)
+
+
+class PublishWheelCommand(ICliCommand):
+
+    def configure_parser(self, parser: ArgumentParser) -> None:
+        pass
+
+    def execute(self) -> None:
+        dist_dir = Path("dist")
+
+        if dist_dir.is_file():
+            raise RuntimeError("invalid dist directory detected")
+
+        shutil.rmtree(dist_dir)
+
+        subprocess.run(["poetry", "build", "--format", "wheel"], check=True)
+        subprocess.run(["poetry", "publish", "--no-interaction"], check=True)

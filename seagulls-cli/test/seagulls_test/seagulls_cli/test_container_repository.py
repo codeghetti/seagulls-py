7c44a076af736e64dfe8c17bc96cf1a83a666350
Lorenzo Pisani <zeelot3k@gmail.com>
2022-02-08 22:37:23 -0800

    some tests for the container repo class

--- seagulls-cli/test/seagulls_test/seagulls_cli/test_container_repository.py
+++ seagulls-cli/test/seagulls_test/seagulls_cli/test_container_repository.py
@@ -1,6 +1,40 @@
-from seagulls.seagulls_cli._container_repository import DiContainerRepository
+import pytest
+
+from seagulls.seagulls_cli._container_repository import DiContainerRepository, \
+    DiContainerNotFoundError, DuplicateDiContainerKeyError
+
+
+class Something:
+    pass
+
+
+class AnotherThing:
+    pass
 
 
 class TestDiContainerRepository:
-    def test_nothing(self) -> None:
-        assert DiContainerRepository
+    _instance: DiContainerRepository
+
+    def setup(self) -> None:
+        self._instance = DiContainerRepository()
+
+    def test_basics(self) -> None:
+        thing = Something()
+        self._instance.register(Something, thing)
+
+        assert self._instance.get(Something) == thing
+
+    def test_missing_container(self) -> None:
+        with pytest.raises(DiContainerNotFoundError):
+            self._instance.get(Something)
+
+    def test_duplicate_container(self) -> None:
+        thing1 = Something()
+        thing2 = AnotherThing()
+        thing3 = Something()
+
+        self._instance.register(Something, thing1)
+        self._instance.register(AnotherThing, thing2)
+
+        with pytest.raises(DuplicateDiContainerKeyError):
+            self._instance.register(Something, thing3)

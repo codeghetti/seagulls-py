7a4cb1ec4e3ec6dc17c68ca9ca4dae05429eba4d
Jennifer <jenniferdeeplearns@gmail.com>
2022-02-10 23:37:19 -0800

    Fix for one of the test files

--- seagulls-space-shooter-demo/test/seagulls_test/space_shooter/test_ship_button.py
+++ seagulls-space-shooter-demo/test/seagulls_test/space_shooter/test_ship_button.py
@@ -3,12 +3,13 @@ from seagulls.space_shooter._ship_button import ShipButton
 
 class TestShipButton:
 
-    ShipButton(
-        "ship",
-        "scene",
-        "asset_manager",
-        "game_controls",
-        "active_scene_manager",
-        "active_ship_manager",
-        "fit_to_screen"
-    )
+    def test_basics(self):
+        ShipButton(
+            "ship",
+            "scene",
+            "asset_manager",
+            "game_controls",
+            "active_scene_manager",
+            "active_ship_manager",
+            "fit_to_screen"
+        )

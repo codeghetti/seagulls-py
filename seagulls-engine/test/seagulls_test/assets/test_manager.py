ce57f4378aff841bdb0b38e8aa2ad3fe0cafd961
Lorenzo Pisani <zeelot3k@gmail.com>
2022-02-12 15:21:26 -0800

    tiny test for the asset manager

--- seagulls-engine/test/seagulls_test/assets/test_manager.py
+++ seagulls-engine/test/seagulls_test/assets/test_manager.py
@@ -1,6 +1,18 @@
+from pathlib import Path
+
+import pytest
+
 from seagulls.assets import AssetManager
 
 
 class TestAssetManager:
     def test_nothing(self) -> None:
         assert AssetManager
+
+    def test_get_path(self) -> None:
+        client = AssetManager(Path("."))
+        expected = Path("pyproject.toml")
+        assert client.get_path("pyproject.toml") == expected
+
+        with pytest.raises(RuntimeError):
+            client.get_path("missing-file.txt")

637aec76f5c0e7c73e4b269692ebde9849cdcc03
Lorenzo Pisani <zeelot3k@gmail.com>
2022-02-16 19:48:50 -0800

    free coverage yay

--- seagulls-engine/src/seagulls/eventing/_interfaces.py
+++ seagulls-engine/src/seagulls/eventing/_interfaces.py
@@ -11,6 +11,8 @@ class IDispatchEvents(Protocol):
     def register_callback(
             self,
             event_type: Type[EventType],
-            callback: EventCallbackType) -> None: ...
+            callback: EventCallbackType) -> None:
+        """Register to be notified of a certain type of events"""
 
-    def trigger_event(self, event: EventType) -> None: ...
+    def trigger_event(self, event: EventType) -> None:
+        """Trigger an event, calling any registered callbacks"""

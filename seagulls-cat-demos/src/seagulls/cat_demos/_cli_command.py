630f65458ccb1ede58afcf722e76db940eb3879c
Lorenzo Pisani <zeelot3k@gmail.com>
2022-11-06 20:41:34 -0800

    more random temporary things

--- seagulls-cat-demos/src/seagulls/cat_demos/_cli_command.py
+++ seagulls-cat-demos/src/seagulls/cat_demos/_cli_command.py
@@ -1,5 +1,55 @@
+from abc import abstractmethod
+
+from typing import Protocol, Iterable, Tuple
+
+from dataclasses import dataclass
+
 from argparse import ArgumentParser
 from seagulls.cli import ICliCommand
+
+
+class IExecutable(Protocol):
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+
+class ITick(Protocol):
+    @abstractmethod
+    def tick(self) -> None:
+        pass
+
+
+class GameSessionStages:
+
+    _stages: Tuple[IExecutable, ...]
+
+    def stages(self) -> Iterable[IExecutable]:
+        return self._stages
+
+
+class GameSession:
+
+    _session_stages: GameSessionStages
+
+    def run(self) -> None:
+        for stage in self._session_stages.stages():
+            stage.execute()
+
+
+@dataclass()
+class SessionBuilderConfig:
+    scene: ITick
+
+
+class GameSessionBuilder:
+    _config: SessionBuilderConfig
+
+    def scene(self, scene: ITick) -> None:
+        self._config.scene = scene
+
+    def build(self) -> GameSession:
+        return GameSession()
 
 
 class GameCliCommand(ICliCommand):
@@ -8,6 +58,9 @@ class GameCliCommand(ICliCommand):
         pass
 
     def execute(self) -> None:
+        builder = GameSessionBuilder()
+        builder.build().run()
+
         session_state_client = GameSessionStateClient()
         session_window_client = GameSessionWindowClient()
         game_input_client = GameInputClient(session_state_client=session_state_client)

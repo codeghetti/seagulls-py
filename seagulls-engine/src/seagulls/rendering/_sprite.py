740da754d36b0ae44298556ec98595879a1efa8d
Z <zeelot3k@gmail.com>
2022-11-23 21:20:37 -0800

    Feature/sprites improvements (#145)
    * spritesheets do not rely on grids any more
    
    * remove old example code

--- seagulls-engine/src/seagulls/rendering/_sprite.py
+++ seagulls-engine/src/seagulls/rendering/_sprite.py
@@ -9,37 +9,19 @@ from ._size import Size
 class SpriteSheet:
     file_path: Path
     resolution: Size
-    grid_size: Size
 
     def get_unit_position(self, coordinates: Position) -> Position:
-        coords = coordinates.get()
-        unit_rez = self.unit_resolution().get()
-
-        x = unit_rez["width"] * coords["x"]
-        y = unit_rez["height"] * coords["y"]
-        return Position({"x": x, "y": y})
-
-    def num_units(self) -> int:
-        grid = self.grid_size.get()
-        return grid["height"] * grid["width"]
-
-    def unit_resolution(self) -> Size:
-        rez = self.resolution.get()
-        grid = self.grid_size.get()
-
-        return Size({
-            "height": int(rez["height"] / grid["height"]),
-            "width": int(rez["width"] / grid["width"]),
-        })
+        return coordinates
 
 
 @dataclass(frozen=True)
 class Sprite:
     sprite_grid: SpriteSheet
     coordinates: Position
+    size: Size
 
     def position(self) -> Position:
         return self.sprite_grid.get_unit_position(self.coordinates)
 
     def resolution(self) -> Size:
-        return self.sprite_grid.unit_resolution()
+        return self.size

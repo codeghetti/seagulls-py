0f506fbc42552fc30821696c9eb57e2be9b73f9c
Lorenzo Pisani <zeelot3k@gmail.com>
2023-01-21 13:49:42 -0800

    start a new resources client

---
+++ seagulls-cat-demos/src/seagulls/cat_demos/engine/v2/_resources.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+class ResourceClient:
+
+    def get_path(self, resource: str) -> Path:
+        # TODO: need a real implementation here.
+        p = Path("../../../../../resources") / resource.lstrip("/")
+        if not p.exists():
+            raise RuntimeError(f"Resource not found: {resource}")
+
+        return p
